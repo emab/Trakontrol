b5f09620802c7b9fb37207002c641447f4b7adb8
Eddy <eddybrown@me.com>
2017-03-07 13:02:41 +0000

    Main Start
    Added a basic splash screen and a blank “Hello World!” activity.

---
+++ app/src/main/java/uk/ac/ncl/b3039134/trakontrol/MainActivity.java
@@ -0,0 +1,13 @@
+package uk.ac.ncl.b3039134.trakontrol;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
